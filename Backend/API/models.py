636a16ee2020dee69dc6fd7b71712ce2dcea3dfa
Netrang <netrang.v@gmail.com>
2024-12-13 23:24:07 +0530

    Bugfix

--- Backend/API/models.py
+++ Backend/API/models.py
@@ -4,9 +4,18 @@ class Courses(models.Model):
     cname=models.CharField(max_length=20,blank=True,null=True)
     des=models.CharField(max_length=50,blank=True,null=True)
     duration=models.IntegerField(blank=True,null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Courses'
+
 class Student(models.Model):
     id=models.IntegerField(primary_key=True)
     course=models.ForeignKey('Courses',models.DO_NOTHING)
     sname=models.CharField(max_length=20,blank=True,null=True)
     email=models.CharField(max_length=255,blank=True,null=True)
     phone=models.CharField(max_length=10,blank=True,null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Student'
